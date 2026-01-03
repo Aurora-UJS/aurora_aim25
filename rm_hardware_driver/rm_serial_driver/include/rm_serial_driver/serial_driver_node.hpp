43c379a4b78d40405a9611eda3004ca01f79624a
xlqmu <3212929002@qq.com>
2026-01-03 17:05:09 +0800

    modified serial protocol to use a packet_based approach

--- rm_hardware_driver/rm_serial_driver/include/rm_serial_driver/serial_driver_node.hpp
+++ rm_hardware_driver/rm_serial_driver/include/rm_serial_driver/serial_driver_node.hpp
@@ -1,4 +1,3 @@
-// Created by Chengfu Zou on 2023.7.1
 // Copyright (C) Aurora Vision Group. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,73 +12,96 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef SERIAL_DRIVER_SERIAL_DRIVER_NODE_HPP_
-#define SERIAL_DRIVER_SERIAL_DRIVER_NODE_HPP_
+#ifndef RM_SERIAL_DRIVER__SERIAL_DRIVER_NODE_HPP_
+#define RM_SERIAL_DRIVER__SERIAL_DRIVER_NODE_HPP_
 
 // std
+#include <atomic>
 #include <memory>
+#include <string>
 #include <thread>
 // ros2
 #include <tf2_ros/transform_broadcaster.h>
 
 #include <geometry_msgs/msg/transform_stamped.hpp>
-#include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+// interfaces
+#include <rm_interfaces/msg/gimbal_cmd.hpp>
+#include <rm_interfaces/msg/serial_receive_data.hpp>
+#include <rm_interfaces/srv/set_mode.hpp>
 // project
-#include "rm_interfaces/msg/gimbal_cmd.hpp"
-#include "rm_interfaces/msg/serial_receive_data.hpp"
-#include "rm_interfaces/srv/set_mode.hpp"
-#include "rm_serial_driver/fixed_packet_tool.hpp"
-#include "rm_serial_driver/protocol.hpp"
-#include "rm_serial_driver/protocol_factory.hpp"
+#include "rm_serial_driver/protocol/crc.hpp"
+#include "rm_serial_driver/protocol/packet_tool.hpp"
+#include "rm_serial_driver/protocol/packet_typedef.hpp"
 #include "rm_serial_driver/transporter_interface.hpp"
 #include "rm_utils/heartbeat.hpp"
 
 namespace aurora::serial_driver {
 
-// Node wrapper for SerialDriver
-// Implementing secondary development through the Protocol class
 class SerialDriverNode : public rclcpp::Node {
 public:
   explicit SerialDriverNode(const rclcpp::NodeOptions &options);
-
-  ~SerialDriverNode();
-
-  void listenLoop();
-
-  void init();
-
-  // Param client to set detect_color
-  struct SetModeClient {
-    SetModeClient(rclcpp::Client<rm_interfaces::srv::SetMode>::SharedPtr p) : ptr(p) {}
-    std::atomic<bool> on_waiting = false;
-    std::atomic<int> mode = 0;
-    rclcpp::Client<rm_interfaces::srv::SetMode>::SharedPtr ptr;
-  };
-  std::unordered_map<std::string, SetModeClient> set_mode_clients_;
-  void setMode(SetModeClient &client, const uint8_t mode);
+  ~SerialDriverNode() override;
 
 private:
-  // Heartbeat
-  HeartBeatPublisher::SharedPtr heartbeat_;
+  // 初始化函数
+  void init();
+  void initTransporter();
+  void initPublishers();
+  void initSubscribers();
+  void initPacketHandlers();
 
-  std::unique_ptr<std::thread> listen_thread_;
-  // Protocol
-  std::unique_ptr<protocol::Protocol> protocol_;
+  // 主循环
+  void listenLoop();
 
-  std::string target_frame_;
+  // 数据包处理器
+  void handleImuData(uint8_t id, const uint8_t *data, size_t len);
 
-  // Subscriptions
-  std::vector<rclcpp::SubscriptionBase::SharedPtr> subscriptions_;
-  // Publisher
-  rclcpp::Publisher<rm_interfaces::msg::SerialReceiveData>::SharedPtr serial_receive_data_pub_;
+  // 回调函数
+  void gimbalCmdCallback(const rm_interfaces::msg::GimbalCmd::SharedPtr msg);
 
-  // Broadcast tf from odom to gimbal_link
-  double timestamp_offset_ = 0;
+  // TF广播
+  void publishGimbalTF(float roll, float pitch, float yaw);
+
+  // 设置模式客户端
+  struct SetModeClient {
+    rclcpp::Client<rm_interfaces::srv::SetMode>::SharedPtr ptr;
+    std::atomic<uint8_t> mode{0};
+    std::atomic<bool> on_waiting{false};
+  };
+  void setMode(SetModeClient &client, uint8_t mode);
+  std::map<std::string, SetModeClient> set_mode_clients_;
+
+  // 传输层
+  std::shared_ptr<TransporterInterface> transporter_;
+  std::unique_ptr<PacketRouter> packet_router_;
+
+  // 发布者
+  rclcpp::Publisher<rm_interfaces::msg::SerialReceiveData>::SharedPtr serial_receive_pub_;
+
+  // 订阅者
+  rclcpp::Subscription<rm_interfaces::msg::GimbalCmd>::SharedPtr gimbal_cmd_sub_;
+
+  // TF广播器
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  // 心跳
+  aurora::HeartBeatPublisher::SharedPtr heartbeat_;
+
+  // 监听线程
+  std::unique_ptr<std::thread> listen_thread_;
+  std::atomic<bool> running_{true};
+
+  // 参数
+  std::string target_frame_;
+  std::string port_name_;
+  double timestamp_offset_{0.0};
+
+  // 状态缓存
+  std::atomic<uint8_t> current_mode_{0};
+  std::atomic<uint8_t> current_color_{0};
 };
 
 }  // namespace aurora::serial_driver
 
-#endif  // SERIAL_DRIVER_SERIAL_DRIVER_NODE_HPP_
+#endif  // RM_SERIAL_DRIVER__SERIAL_DRIVER_NODE_HPP_
--- rm_hardware_driver/rm_serial_driver/src/serial_driver_node.cpp
+++ rm_hardware_driver/rm_serial_driver/src/serial_driver_node.cpp
@@ -1,4 +1,3 @@
-// Created by Chengfu Zou on 2023.7.1
 // Copyright (C) Aurora Vision Group. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,145 +15,189 @@
 #include "rm_serial_driver/serial_driver_node.hpp"
 
 #include <tf2/LinearMath/Matrix3x3.h>
-// std
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <Eigen/Geometry>
 #include <chrono>
 #include <cstdint>
+#include <functional>
 #include <memory>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <thread>
-// ros2
-#include <Eigen/Geometry>
-#include <rclcpp/rclcpp.hpp>
-// project
+
 #include "rm_serial_driver/uart_transporter.hpp"
 #include "rm_utils/logger/log.hpp"
 #include "rm_utils/math/utils.hpp"
 
 namespace aurora::serial_driver {
+
 SerialDriverNode::SerialDriverNode(const rclcpp::NodeOptions &options)
 : Node("serial_driver", options) {
   AURORA_REGISTER_LOGGER("serial_driver", "~/aurora2026-log", INFO);
 
-  // Task thread
+  // 延迟初始化，因为构造函数中shared_from_this()不可用
   listen_thread_ = std::make_unique<std::thread>(&SerialDriverNode::listenLoop, this);
-}
-
-void SerialDriverNode::init() {
-  AURORA_INFO("serial_driver", "Initializing SerialDriverNode!");
-  // Init
-  target_frame_ = this->declare_parameter("target_frame", "odom");
-  std::string port_name = this->declare_parameter("port_name", "/dev/ttyUSB0");
-  std::string protocol_type = this->declare_parameter("protocol", "infantry");
-  bool enable_data_print = this->declare_parameter("enable_data_print", false);
-  // Create Protocol
-  protocol_ = ProtocolFactory::createProtocol(protocol_type, port_name, enable_data_print);
-  if (protocol_ == nullptr) {
-    AURORA_FATAL("serial_driver", "Failed to create protocol with type: {}", protocol_type);
-    rclcpp::shutdown();
-    return;
-  }
-  AURORA_INFO(
-    "serial_driver", "Protocol has been created with type: {}, port: {}", protocol_type, port_name);
-
-  // Subscriptions
-  subscriptions_ = protocol_->getSubscriptions(this->shared_from_this());
-  for (auto sub : subscriptions_) {
-    AURORA_INFO("serial_driver", "Subscribe to topic: {}", sub->get_topic_name());
-  }
-  // Publisher
-  serial_receive_data_pub_ = this->create_publisher<rm_interfaces::msg::SerialReceiveData>(
-    "serial/receive", rclcpp::SensorDataQoS());
-
-  // TF broadcaster
-  timestamp_offset_ = this->declare_parameter("timestamp_offset", 0.0);
-  tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-  // Param client
-  for (auto client : protocol_->getClients(this->shared_from_this())) {
-    std::string name = client->get_service_name();
-    set_mode_clients_.emplace(name, client);
-    AURORA_INFO("serial_driver", "Create client for service: {}", name);
-  }
-
-  // Heartbeat
-  heartbeat_ = HeartBeatPublisher::create(this);
-
-  AURORA_INFO("serial_driver", "SerialDriverNode has been initialized!");
 }
 
 SerialDriverNode::~SerialDriverNode() {
   AURORA_INFO("serial_driver", "Destroy SerialDriverNode!");
-  rclcpp::shutdown();
-  if (listen_thread_ != nullptr) {
+  running_.store(false);
+  if (listen_thread_ != nullptr && listen_thread_->joinable()) {
     listen_thread_->join();
   }
 }
 
-void SerialDriverNode::listenLoop() {
-  if (protocol_ == nullptr) {
-    // Lazy init because shared_from_this() is not available in constructor
-    init();
+void SerialDriverNode::init() {
+  AURORA_INFO("serial_driver", "Initializing SerialDriverNode with new protocol!");
+
+  // 声明参数
+  target_frame_ = this->declare_parameter("target_frame", "odom");
+  port_name_ = this->declare_parameter("port_name", "/dev/ttyUSB0");
+  timestamp_offset_ = this->declare_parameter("timestamp_offset", 0.0);
+
+  // 初始化各组件
+  initTransporter();
+  initPublishers();
+  initSubscribers();
+  initPacketHandlers();
+
+  // TF广播器
+  tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+  // 心跳
+  heartbeat_ = aurora::HeartBeatPublisher::create(this);
+
+  AURORA_INFO("serial_driver", "SerialDriverNode initialized with port: {}", port_name_);
+}
+
+void SerialDriverNode::initTransporter() {
+  transporter_ = std::make_shared<UartTransporter>(port_name_);
+  if (!transporter_->open()) {
+    AURORA_ERROR("serial_driver", "Failed to open serial port: {}", port_name_);
   }
+  packet_router_ = std::make_unique<PacketRouter>(transporter_);
+}
 
-  rm_interfaces::msg::SerialReceiveData receive_data;
-  while (rclcpp::ok()) {
-    if (protocol_->receive(receive_data)) {
-      receive_data.header.stamp = this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
-      receive_data.header.frame_id = target_frame_;
-      serial_receive_data_pub_->publish(receive_data);
+void SerialDriverNode::initPublishers() {
+  serial_receive_pub_ = this->create_publisher<rm_interfaces::msg::SerialReceiveData>(
+    "serial/receive", rclcpp::SensorDataQoS());
+}
 
-      for (auto &[service_name, client] : set_mode_clients_) {
-        if (client.mode.load() != receive_data.mode && !client.on_waiting.load()) {
-          setMode(client, receive_data.mode);
-        }
-      }
+void SerialDriverNode::initSubscribers() {
+  gimbal_cmd_sub_ = this->create_subscription<rm_interfaces::msg::GimbalCmd>(
+    "serial/gimbal_cmd",
+    rclcpp::SensorDataQoS(),
+    std::bind(&SerialDriverNode::gimbalCmdCallback, this, std::placeholders::_1));
+}
 
-      geometry_msgs::msg::TransformStamped t;
-      timestamp_offset_ = this->get_parameter("timestamp_offset").as_double();
-      t.header.stamp = this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
-      t.header.frame_id = target_frame_;
-      t.child_frame_id = "gimbal_link";
-      auto roll = receive_data.roll * M_PI / 180.0;
-      auto pitch = -receive_data.pitch * M_PI / 180.0;
-      auto yaw = receive_data.yaw * M_PI / 180.0;
-      tf2::Quaternion q;
-      q.setRPY(roll, pitch, yaw);
-      t.transform.rotation = tf2::toMsg(q);
-      tf_broadcaster_->sendTransform(t);
+void SerialDriverNode::initPacketHandlers() {
+  using namespace std::placeholders;
 
-      // odom_rectify: 转了roll角后的坐标系
-      Eigen::Quaterniond q_eigen(q.w(), q.x(), q.y(), q.z());
-      Eigen::Vector3d rpy = utils::getRPY(q_eigen.toRotationMatrix());
-      q.setRPY(rpy[0], 0, 0);
-      t.header.frame_id = target_frame_;
-      t.child_frame_id = target_frame_ + "_rectify";
-      tf_broadcaster_->sendTransform(t);
-    } else {
-      auto error_message = protocol_->getErrorMessage();
-      error_message = error_message.empty() ? "unknown" : error_message;
-      AURORA_WARN("serial_driver", "Failed to reveive packet! error message :{}", error_message);
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  // 注册IMU数据包处理器
+  packet_router_->registerHandler(ID_IMU,
+                                  std::bind(&SerialDriverNode::handleImuData, this, _1, _2, _3));
+
+  AURORA_INFO("serial_driver", "Packet handlers registered");
+}
+
+void SerialDriverNode::listenLoop() {
+  // 延迟初始化
+  init();
+
+  while (running_.load() && rclcpp::ok()) {
+    // 接收并分发数据包
+    if (!packet_router_->receiveAndDispatch()) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
   }
 }
 
-void SerialDriverNode::setMode(SetModeClient &client, const uint8_t mode) {
+void SerialDriverNode::handleImuData(uint8_t id, const uint8_t *data, size_t len) {
+  (void)id;
+  (void)len;
+
+  auto packet = copyFromBuffer<ReceiveImuData>(data);
+
+  // 提取数据
+  float roll = packet.data.roll;
+  float pitch = packet.data.pitch;
+  float yaw = packet.data.yaw;
+
+  // 发布ROS消息
+  rm_interfaces::msg::SerialReceiveData msg;
+  msg.header.stamp = this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
+  msg.header.frame_id = target_frame_;
+  msg.roll = roll;
+  msg.pitch = pitch;
+  msg.yaw = yaw;
+  msg.mode = current_mode_.load();
+  msg.bullet_speed = 0.0f;  // 从 robot_status 获取
+
+  serial_receive_pub_->publish(msg);
+
+  // 广播TF
+  publishGimbalTF(roll, pitch, yaw);
+}
+
+void SerialDriverNode::gimbalCmdCallback(const rm_interfaces::msg::GimbalCmd::SharedPtr msg) {
+  SendGimbalCmd packet;
+  packet.frame_header.id = ID_GIMBAL_CMD;
+
+  packet.data.pitch = static_cast<float>(msg->pitch);
+  packet.data.yaw = static_cast<float>(msg->yaw);
+  packet.data.distance = static_cast<float>(msg->distance);
+  packet.data.fire_advice = msg->fire_advice ? 1 : 0;
+  packet.data.tracking = 1;
+
+  packet_router_->send(packet);
+}
+
+void SerialDriverNode::publishGimbalTF(float roll, float pitch, float yaw) {
+  timestamp_offset_ = this->get_parameter("timestamp_offset").as_double();
+
+  geometry_msgs::msg::TransformStamped t;
+  t.header.stamp = this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
+  t.header.frame_id = target_frame_;
+  t.child_frame_id = "gimbal_link";
+
+  // 角度转换
+  auto roll_rad = roll * M_PI / 180.0;
+  auto pitch_rad = -pitch * M_PI / 180.0;
+  auto yaw_rad = yaw * M_PI / 180.0;
+
+  tf2::Quaternion q;
+  q.setRPY(roll_rad, pitch_rad, yaw_rad);
+  t.transform.rotation = tf2::toMsg(q);
+  tf_broadcaster_->sendTransform(t);
+
+  // odom_rectify: 转了roll角后的坐标系
+  Eigen::Quaterniond q_eigen(q.w(), q.x(), q.y(), q.z());
+  Eigen::Vector3d rpy = utils::getRPY(q_eigen.toRotationMatrix());
+  q.setRPY(rpy[0], 0, 0);
+  t.header.frame_id = target_frame_;
+  t.child_frame_id = target_frame_ + "_rectify";
+  tf_broadcaster_->sendTransform(t);
+}
+
+void SerialDriverNode::setMode(SetModeClient &client, uint8_t mode) {
   using namespace std::chrono_literals;
 
   std::string service_name = client.ptr->get_service_name();
-  // Wait for service
+
   while (!client.ptr->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
       AURORA_ERROR(
-        "serial_driver", "Interrupted while waiting for the service {}. Exiting.", service_name);
+        "serial_driver", "Interrupted while waiting for service {}. Exiting.", service_name);
       return;
     }
-    AURORA_INFO("serial_driver", "Service {} not available, waiting again...", service_name);
+    AURORA_INFO("serial_driver", "Service {} not available, waiting...", service_name);
   }
+
   if (!client.ptr->service_is_ready()) {
-    AURORA_WARN("serial_driver", "Service: {} is not available!", service_name);
+    AURORA_WARN("serial_driver", "Service {} is not available!", service_name);
     return;
   }
-  // Send request
+
   auto req = std::make_shared<rm_interfaces::srv::SetMode::Request>();
   req->mode = mode;
 
@@ -171,7 +214,4 @@ void SerialDriverNode::setMode(SetModeClient &client, const uint8_t mode) {
 }  // namespace aurora::serial_driver
 
 #include "rclcpp_components/register_node_macro.hpp"
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be discoverable when its library
-// is being loaded into a running process.
 RCLCPP_COMPONENTS_REGISTER_NODE(aurora::serial_driver::SerialDriverNode)
